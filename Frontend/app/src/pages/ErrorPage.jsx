5f59cafde7e4974c7d149c6259eb93de649fb6ae
Subat Ahsan <subat.ahsan18@gmail.com>
2025-06-13 13:02:34 -0400

    Added navbar to error page

--- Frontend/app/src/pages/ErrorPage.jsx
+++ Frontend/app/src/pages/ErrorPage.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
+import NavBar from "./NavBar"
 
 export default function ErrorPage() {
   return (
+    <>
+    <NavBar />
     <div style={{ padding: 20 }}>
       <h1>Oops! Something went wrong.</h1>
     </div>
+    </>
   );
 }
